b6ddba73dbac32b69fb08f10423f66e761226418
Lilitha27 <lilithakganya@gmail.com>
2019-10-07 09:23:22 +0200

    added new navigation bar

--- js/js.js
+++ js/js.js
@@ -33,3 +33,13 @@ $icon.onclick = () => {
     iterations: Infinity
   });
 }
+
+// navigation bar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
