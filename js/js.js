a90456ee6ebead48ba5d51592b9aa2f1fddd7451
Lilitha27 <lilithakganya@gmail.com>
2019-10-09 10:35:59 +0200

    added mail to

--- js/js.js
+++ js/js.js
@@ -17,7 +17,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 9000); // Change image every 9 seconds
+  setTimeout(showSlides, 7000); // Change image every 7 seconds
 }
 // arrow animation
 // const $icon = document.querySelector('.icon');
